c9c4fdfdb11ebf9b26206c3ac54ce2ae175966fc
innight <joaosemedo16@hotmail.com>
2022-12-10 21:42:19 +0000

    Corrigido }

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -10,7 +10,7 @@ namespace GradeBook.GradeBooks
         public StandardGradeBook(string name) : base(name)
         {
             Name = name;
+            Type = Enums.GradeBookType.Standard;
         }
     }
-}
 }
